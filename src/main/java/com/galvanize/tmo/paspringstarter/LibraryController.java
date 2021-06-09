1ef049e6dbd1635be86f5e3698d30f6fd0b38837
Pete Silva <peter.silva@galvanize.com>
2021-06-09 13:52:16 -0600

    add basic GET ALL route

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,12 +1,15 @@
 package com.galvanize.tmo.paspringstarter;
 
 import org.apache.catalina.connector.Response;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Properties;
 
 @RestController
@@ -23,5 +26,15 @@ public class LibraryController {
     public ResponseEntity<Book> postBook(@RequestBody Book book){
         book.setId(1);
         return ResponseEntity.status(201).body(book);
+    }
+
+    @GetMapping("/books")
+    public ArrayList<Book> getBooks() {
+        Book b = new Book("author", "title", 1979);
+        ArrayList<Book> books = new ArrayList();
+        books.add(b);
+        books.add(b);
+        books.add(b);
+        return books;
     }
 }
